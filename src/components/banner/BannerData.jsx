4bb3f8868830d3c3420affb943728bdc9ee1d7b1
lucas singh <lucas.singh10@gmail.com>
2020-11-30 14:50:00 -0300

    cambios banner adidas

---
+++ src/components/banner/BannerData.jsx
@@ -0,0 +1,17 @@
+export const BannerDataAdidas = [
+    {
+        // title: 'Adidas PulseBoost',
+        urlImg: 'assets/banners/adidas-banner-1.jpg',
+        // textInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+    },
+    {
+        // title: 'Adidas Run 90s',
+        urlImg: 'assets/banners/adidas-banner-2.jpg',
+        // textInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+    },
+    {
+        // title: 'Adidas Ultraboost',
+        urlImg: 'assets/banners/adidas-banner-3.jpg',
+        // textInfo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
+    }
+]
